289eac3be42bd8c91409a8ac8bdeca1ecefc4dad
EmonHossainDiza <emon_live0@yahoo.com>
2018-01-17 23:59:44 +0600

    emon

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,6 +6,24 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_view');
+
+                $ip = $this->input->ip_address();
+        $result = json_decode(file_get_contents("http://freegeoip.net/json/{$ip}"));
+        echo $result->ip;
+        echo $result->country_name;
+        echo $result->city;
+        echo $result->time_zone;
+
+        $this->load->view('welcome_view',$result);
 	}
 }
+
+//
+//$ip = $this->input->ip_address();
+//$result = json_decode(file_get_contents("http://ip-api.io/json/{$ip}"));
+//echo $result->ip;
+//echo $result->country_name;
+//echo $result->city;
+//echo $result->time_zone;
+//
+//$this->load->view('welcome_view',$result);
