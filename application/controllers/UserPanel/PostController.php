f3e82d6ad903896ba1af07a7b55737c9dad9f7b8
DelwarSumon <dssumon0@gmail.com>
2018-02-19 06:09:32 +0600

    delwar

--- application/controllers/UserPanel/PostController.php
+++ application/controllers/UserPanel/PostController.php
@@ -1,28 +1,22 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class PostController extends CI_Controller
 {
-
     public function __construct()
     {
         parent::__construct();
     }
     public function newPost()
     {
-
     }
     public function getCategory()
     {
-
     }
     public function requiredInfoByCategory()
     {
         $catId=$this->input->post('catId');
         $this->load->model('UserPanel/UserModel');
         $catInfo=$this->UserModel->getCategoryNameStatus($catId);
-
-        $data[] = $this->UserModel->getCategoryNameStatus($catId);
         
         if ($catInfo->post_category_status =="1"){
             $this->load->view('UserPanel/newFamilyPost');
@@ -45,12 +39,54 @@ class PostController extends CI_Controller
         else{
             echo '';
         }
-
     }
-
     public function insertFmailyPost()
     {
-        
-
+        $data1=array(
+            'no_of_room' => $this->input->post('room_number'),
+            'no_of_bathroom' => $this->input->post('bathroom_number'),
+            'no_of_balcony' => $this->input->post('balcony_number'),
+            'area' => $this->input->post('area_sqft'),
+            'floor' => $this->input->post('floor_number'),
+            'negotiable_rent' => $this->input->post('negotiable_check'),
+            'installment_rent' => $this->input->post('installment_check')
+        );
+            $this->load->model('UserPanel/PostModel');
+           $SpecificDetailsId= $this->PostModel->insertSpecificDetails($data1);
+        $data2=array(
+            'cc_camera' => $this->input->post('cc_camera'),
+            'car_parking' => $this->input->post('car_parking'),
+            'lift' => $this->input->post('lift'),
+            'generator' => $this->input->post('generator'),
+            'play_ground' => $this->input->post('play_ground'),
+            'water_gas_eclectricity' => $this->input->post('water_gas_eclectricity'),
+            'security_guard' => $this->input->post('security_guard'),
+            'geyser' => $this->input->post('geyser')
+        );
+        $OtherBenefitId= $this->PostModel->insertOtherBenefit($data2);
+        $data3=array(
+            'post_category_id' => $this->input->post('category_id'),
+            'post_title' => $this->input->post('post_title'),
+            'post_phone' => $this->input->post('contact_number'),
+            'post_date_from' => $this->input->post('rent_date'),
+            'post_price' => $this->input->post('rent_amount'),
+            'post_advance_price' => $this->input->post('advance_rent_amount'),
+            'post_description' => $this->input->post('post_description'),
+            'post_status' => "1",
+            'post_availability' => "1",
+            'post_specific_details_id' => $SpecificDetailsId,
+            'post_other_benefit_id' => $OtherBenefitId,
+        );
+        $this->load->library('upload');
+        $config['upload_path'] = './tolet_post/real_img/';
+        $config['allowed_types'] = 'gif|jpg|png|jpeg';
+        $config['max_size'] = '2048';
+        $this->upload->initialize($config);
+        $this->upload->do_upload('post_image');
+        $images = $this->upload->data();
+        $photo= $images['file_name'];
+        thumb('./tolet_post/real_img/'.$photo,'100','100');
+        $post= $this->PostModel->insertPost($data3);
+        redirect('New_post');
     }
 }
